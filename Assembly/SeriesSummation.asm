44fc5b575d6df7a79a00a851c8e85bd181672b19
Manik Hosen <rahathossenmanik@gmail.com>
2020-02-25 18:15:13 +0600

    Sum Update

--- Assembly/SeriesSummation.asm
+++ Assembly/SeriesSummation.asm
@@ -1,7 +1,6 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-X DB 80 DUP
 N DB 0
 
 .CODE      
@@ -9,58 +8,35 @@ MOV AX, @DATA
 MOV DS, AX
 
 MAIN PROC
-    MOV SI, 0
     MOV AH, 1
     INT 21H
     MOV N, AL
-    MOV AL, 0
+    SUB N, 30H
     
-    FOR: INT 21H
-        CMP AL, N
-        JG EXIT
-        
-        INC SI
-        JMP FOR
-        
-    EXIT: DEC SI
-
-    SORT:
-    MOV BX, SI
-    
-    EXTER:
-        CMP SI, 0
-        JL END
-        MOV DI, SI
-        INER: CMP DI, 0
-            JL EXEND
-            MOV CL, X[DI]
-            CMP X[SI], CL
-            JL MOVE
-            INEND:
-            DEC DI
-            JMP INER
-        EXEND: DEC SI
-        JMP EXTER
-        
-    MOVE: XCHG CL, X[SI]
-        MOV X[DI], CL
-        JMP INEND
-    
-    END: MOV SI, 0
     MOV AH, 2
     MOV DL, 0AH
     INT 21H
     MOV DL, 0DH
     INT 21H
-     
-    LOOPING:
-        CMP BX, SI
-        JL FINISH
-        MOV DL, X[SI] 
+        
+    MOV CX, 1
+    MOV AX, 1
+    MOV BX, 1
+    
+    FOR:
+        CMP CL, N
+        JG EXIT
+        MOV BX, CX
+        MUL BX
+        INC CX
+    JMP FOR
+        
+    EXIT:
+        ADD AX, 30H
+        MOV DL, AL
+        MOV AH, 2
         INT 21H
-        INC SI
-        JMP LOOPING
-    FINISH: MOV AH, 4CH
+        MOV AH, 4CH
         INT 21H
 MAIN ENDP
 END MAIN
