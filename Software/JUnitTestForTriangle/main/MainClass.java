3b0387148cc010c9d22032789ada2d70f729b6c3
Manik Hosen <rahathossenmanik@gmail.com>
2020-02-26 17:30:20 +0600

    JUnit Test

--- Software/JUnitTestForTriangle/main/MainClass.java
+++ Software/JUnitTestForTriangle/main/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 		if(x<1||x>99 || y<1||y>99 || z<1||z>99)return "INVALID INPUT";
 		else if(x+y<=z || y+z<=x || x+z<=y)return "NOT A TRIANGLE";
 		else if(x==y && x==z)return "EQUILATERAL";
-		else if((x==y && x!=z)||(x==z && x!=y))return "ISOSCELES";
+		else if((x==y && x!=z)||(x==z && x!=y)||(y==z && x!=y))return "ISOSCELES";
 		else if(x!=y && x!=z && y!=z)return "SCALENE";
 		else return "INVALID INPUT";
 	}
